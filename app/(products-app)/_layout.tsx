eee78aa1d31811bd4c04013ed7fabbdccee80ca3
Leonardo <leonardo38361@gmail.com>
2025-12-20 21:36:37 -0300

    Estructura inicial

--- app/(products-app)/_layout.tsx
+++ app/(products-app)/_layout.tsx
@@ -1,13 +1,9 @@
+import { useColorScheme } from '@/presentation/theme/hooks/use-color-scheme.web';
 import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
 import { Stack } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
 import 'react-native-reanimated';
 
-import { useColorScheme } from '@/hooks/use-color-scheme';
-
-export const unstable_settings = {
-  anchor: '(tabs)',
-};
 
 export default function RootLayout() {
   const colorScheme = useColorScheme();
@@ -15,8 +11,6 @@ export default function RootLayout() {
   return (
     <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
       <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="modal" options={{ presentation: 'modal', title: 'Modal' }} />
       </Stack>
       <StatusBar style="auto" />
     </ThemeProvider>
