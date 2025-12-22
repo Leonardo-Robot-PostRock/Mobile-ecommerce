5514c08a8dbb78d7d005171e526e7d4eef0d125f
Leonardo <leonardo38361@gmail.com>
2025-12-21 21:29:43 -0300

    creación de useAuth store e intalación de zustand

---
+++ presentation/auth/store/useAuthStore.ts
@@ -0,0 +1,32 @@
+import { User } from "@/core/auth/interface/user";
+import { create } from "zustand";
+
+export type AuthStatus = 'authenticated' | 'unauthenticated' | 'checking';
+
+export interface AuthState {
+    status: AuthStatus;
+    token?: string;
+    user?: User;
+
+    login: (email: string, password: string) => Promise<boolean>;
+    checkStatus: () => Promise<void>;
+    logout: () => Promise<void>;
+}
+
+export const useAuthStore = create<AuthState>()((set) => ({
+    //Properties, variables or states
+    status: 'checking',
+    token: undefined,
+    user: undefined,
+
+    //Actions, methods or functions
+    login: async (email: string, password: string) => {
+        return true;
+    },
+
+    checkStatus: async () => {
+    },
+
+    logout: async () => {
+    }
+}))
