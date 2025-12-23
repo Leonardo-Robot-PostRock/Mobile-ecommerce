382fd870c6f616aa41bc8d967488076341ba3523
Leonardo <leonardo38361@gmail.com>
2025-12-23 09:59:56 -0300

    conexión de acciones con el store

--- presentation/auth/store/useAuthStore.ts
+++ presentation/auth/store/useAuthStore.ts
@@ -1,3 +1,4 @@
+import { authCheckStatus, authLogin } from "@/core/auth/api/productsApi";
 import { User } from "@/core/auth/interface/user";
 import { create } from "zustand";
 
@@ -11,22 +12,45 @@ export interface AuthState {
     login: (email: string, password: string) => Promise<boolean>;
     checkStatus: () => Promise<void>;
     logout: () => Promise<void>;
+    changeStatus: (token?: string, user?: User) => boolean;
 }
 
-export const useAuthStore = create<AuthState>()((set) => ({
+export const useAuthStore = create<AuthState>()((set, get) => ({
     //Properties, variables or states
     status: 'checking',
     token: undefined,
     user: undefined,
 
-    //Actions, methods or functions
-    login: async (email: string, password: string) => {
+    changeStatus: (token?: string, user?: User) => {
+        if (!token || !user) {
+            set({ status: 'unauthenticated', token: undefined, user: undefined });
+            //TODO: llamar logout
+
+            return false;
+        }
+        
+        set({
+            status: 'authenticated',
+            token,
+            user
+        })
+
         return true;
     },
 
+    //Actions, methods or functions
+    login: async (email: string, password: string) => {
+        const { token, user } = await authLogin(email, password);
+        return get().changeStatus(token, user);
+    },
+
     checkStatus: async () => {
+        const res = await authCheckStatus();
+        get().changeStatus(res?.token, res?.user)
     },
 
     logout: async () => {
+        //Clear token en el secure storage
+        set({ status: 'unauthenticated', token: undefined, user: undefined })
     }
 }))
