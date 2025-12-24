90b67e36df6344daeda4d3a40225e8872c0b33b6
Leonardo <leonardo38361@gmail.com>
2025-12-24 10:52:14 -0300

    pantalla de login

--- app/auth/login/index.tsx
+++ app/auth/login/index.tsx
@@ -1,11 +1,42 @@
-import { ThemedText } from '@/presentation/theme/components/ThemedText'
-import { View } from 'react-native'
+import { ThemedText } from '@/presentation/theme/components/ThemedText';
+import ThemedTextInput from '@/presentation/theme/components/ThemedTextInput';
+import { KeyboardAvoidingView, ScrollView, useWindowDimensions, View } from 'react-native';
 
 const LoginScreen = () => {
+    const { height } = useWindowDimensions();
+
     return (
-        <View>
-            <ThemedText>LoginScreen</ThemedText>
-        </View>
+        <KeyboardAvoidingView
+            behavior='padding'
+            style={{ flex: 1 }}
+        >
+            <ScrollView
+                style={{ paddingHorizontal: 40 }}
+            >
+                <View style={{
+                    paddingTop: height * 0.35
+                }}>
+
+                    <ThemedText type='title'>Ingresar</ThemedText>
+                    <ThemedText style={{ color: 'grey' }}>Por favor ingrese para continuar</ThemedText>
+                </View>
+
+                <View style={{ marginTop: 20 }}>
+                    <ThemedTextInput
+                        placeholder='Correo electrónico'
+                        keyboardType='email-address'
+                        autoCapitalize='none'
+                        icon='mail-outline'
+                    />
+                    <ThemedTextInput
+                        placeholder='Contraseña'
+                        secureTextEntry
+                        autoCapitalize='none'
+                        icon='lock-closed-outline'
+                    />
+                </View>
+            </ScrollView>
+        </KeyboardAvoidingView>
     )
 }
 
