f517272404e17740361ab2f4d563ea4765a2e3b5
Leonardo <leonardo38361@gmail.com>
2025-12-25 13:13:32 -0300

    Creación de button personalizado

---
+++ presentation/theme/components/ThemedButton.tsx
@@ -0,0 +1,48 @@
+import { Ionicons } from '@expo/vector-icons';
+
+import { Pressable, PressableProps, StyleSheet, Text } from 'react-native';
+import { useThemeColor } from '../hooks/useThemeColor';
+
+
+interface Props extends PressableProps {
+    icon?: keyof typeof Ionicons.glyphMap,
+    children: string,
+}
+
+const ThemedButton = ({ children, icon, ...rest }: Props) => {
+    const primaryColor = useThemeColor({}, 'primary');
+
+    return (
+        <Pressable {...rest} style={({ pressed }) => [
+            { backgroundColor: pressed ? primaryColor + '90' : primaryColor },
+            styles.button,
+        ]}>
+            <Text style={{ color: 'white' }}>{children}</Text>
+
+            {icon && (
+                <Ionicons
+                    name={icon}
+                    size={24}
+                    color='white'
+                    style={{ marginHorizontal: 5 }}
+                />
+            )}
+        </Pressable>
+    )
+}
+
+
+export default ThemedButton
+
+const styles = StyleSheet.create({
+    button: {
+        alignItems: 'center',
+        borderRadius: 5,
+        height: 50,
+        width: '100%',
+        flexDirection: 'row',
+        justifyContent: 'center',
+        paddingHorizontal: 10,
+        paddingVertical: 15
+    }
+})
