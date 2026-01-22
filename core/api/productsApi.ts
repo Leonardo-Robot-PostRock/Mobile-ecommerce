58a2b58134f236c05e83e0e4746477357fa9c651
Leonardo <leonardo38361@gmail.com>
2026-01-22 13:53:42 -0300

    feat(api): add centralized productsApi axios instance with auth interceptors

---
+++ core/api/productsApi.ts
@@ -0,0 +1,78 @@
+import { SecureStorageAdapter } from '@/helpers/adapter/secure-storage.adapter';
+import axios from 'axios';
+import { Platform } from 'react-native';
+import { AuthResponse } from '../actions/auth-actions';
+import { User } from '../interface/user';
+
+// TODO: conectar mediante envs vars, Android e IOS
+const STAGE = process.env.EXPO_PUBLIC_STAGE || 'dev';
+
+export const API_URL = (STAGE === 'prod')
+    ? process.env.EXPO_PUBLIC_API_URL
+    : (Platform.OS) === 'ios'
+        ? process.env.EXPO_PUBLIC_API_URL_IOS
+        : process.env.EXPO_PUBLIC_API_URL_ANDROID;
+
+console.log({ STAGE, [Platform.OS]: API_URL })
+
+const productsApi = axios.create({
+    baseURL: API_URL,
+})
+
+// Interceptores de requests
+
+productsApi.interceptors.request.use(async (config) => {
+    //Verificar si tenemos un token en secure storage
+    const token = await SecureStorageAdapter.getItem('token');
+
+    if (token) {
+        config.headers.Authorization = `Bearer ${token}`;
+    }
+
+    return config;
+})
+
+
+export default productsApi;
+
+const returnUserToken = (data: AuthResponse): { user: User, token: string } => {
+    const { token, ...user } = data;
+
+    return { user, token }
+
+}
+
+export const authLogin = async (email: string, password: string) => {
+    email = email.toLocaleLowerCase();
+
+    try {
+        const { data } = await productsApi.post<AuthResponse>('/auth/login', { email, password });
+
+        return returnUserToken(data);
+    } catch (error) {
+        throw new Error('User and/or password are not valid');
+    }
+}
+
+export const authCheckStatus = async () => {
+    try {
+        const { data } = await productsApi.get<AuthResponse>('/auth/check-status');
+
+        return returnUserToken(data);
+    } catch (error) {
+        return null;
+    }
+}
+
+//TODO: Tarea: hacer el register
+export const authRegister = async (email: string, password: string, fullName: string,) => {
+    email = email.toLocaleLowerCase();
+
+    try {
+        const { data } = await productsApi.post<AuthResponse>('/auth/register', { email, password, fullName });
+
+        return returnUserToken(data)
+    } catch (error) {
+        throw new Error('User and/or password are not valid');
+    }
+}
