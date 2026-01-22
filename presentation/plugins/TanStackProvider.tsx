e2861a4f4f2aea2d50dee929740c28faa80c6e68
Leonardo <leonardo38361@gmail.com>
2026-01-22 20:46:20 -0300

    feat(core): add TanStackProvider (QueryClientProvider)

---
+++ presentation/plugins/TanStackProvider.tsx
@@ -0,0 +1,21 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import React, { PropsWithChildren } from 'react';
+
+const TanStackProvider = ({ children }: PropsWithChildren) => {
+
+    const queryClient = new QueryClient({
+        defaultOptions: {
+            queries: {
+                retry: false
+            }
+        }
+    });
+
+    return (
+        <QueryClientProvider client={queryClient}>
+            {children}
+        </QueryClientProvider>
+    )
+};
+
+export default TanStackProvider;
