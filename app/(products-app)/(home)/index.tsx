c6ac9008ad9d05b778335a7125e6fb2ad0e8e7f2
Leonardo <leonardo38361@gmail.com>
2026-01-23 17:20:15 -0300

    feat: agregar hook useProducts (consulta infinita de productos)

--- app/(products-app)/(home)/index.tsx
+++ app/(products-app)/(home)/index.tsx
@@ -1,4 +1,3 @@
-import { ThemedText } from '@/presentation/theme/components/ThemedText';
 import { useThemeColor } from '@/presentation/theme/hooks/useThemeColor';
 import React from 'react';
 import { View } from 'react-native';
@@ -7,10 +6,8 @@ const HomeScreen = () => {
     const primary = useThemeColor({}, 'primary')
 
     return (
-        <View style={{ paddingTop: 100, paddingHorizontal: 20 }}>
-            <ThemedText style={{ color: primary, fontFamily: 'Kanit-Bold' }}>HomeScreen</ThemedText>
-            <ThemedText style={{ fontFamily: 'Kanit-Regular' }}>HomeScreen</ThemedText>
-            <ThemedText style={{ fontFamily: 'Kanit-Thin' }}>HomeScreen</ThemedText>
+        <View style={{  paddingHorizontal: 10 }}>
+            
         </View>
     )
 }
