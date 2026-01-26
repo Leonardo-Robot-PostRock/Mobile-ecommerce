5cb3d6052dd5975ba9de68ea72c4b78ad0fac581
Leonardo <leonardo38361@gmail.com>
2026-01-26 17:18:44 -0300

    feat(product): add product form UI, keyboard handling and header icon

--- app/(products-app)/product/[id].tsx
+++ app/(products-app)/product/[id].tsx
@@ -1,10 +1,61 @@
-import { Text, View } from 'react-native'
+import ThemedTextInput from '@/presentation/theme/components/ThemedTextInput';
+import { ThemedView } from '@/presentation/theme/components/ThemedView';
+import { Ionicons } from '@expo/vector-icons';
+import { useNavigation } from 'expo-router';
+import { useEffect } from 'react';
+import { KeyboardAvoidingView, Platform } from 'react-native';
+import { ScrollView } from 'react-native-gesture-handler';
 
 const ProductScreen = () => {
+
+  const navigation = useNavigation();
+
+  useEffect(() => {
+    navigation.setOptions({
+      headerRight: () => <Ionicons name="camera-outline" size={25} />
+    })
+  }, [])
+
   return (
-    <View>
-      <Text>ProductScreen</Text>
-    </View>
+    <KeyboardAvoidingView
+      behavior={Platform.OS === 'ios' ? 'padding' : undefined}
+    >
+      <ScrollView>
+        <ThemedView style={{ marginHorizontal: 10, marginTop: 10 }}>
+          <ThemedTextInput placeholder='Título' style={{ marginVertical: 5 }} />
+
+          <ThemedTextInput placeholder='Slug' style={{ marginVertical: 5 }} />
+
+          <ThemedTextInput
+            placeholder='Descripción'
+            multiline
+            numberOfLines={4}
+            style={{ marginVertical: 5 }}
+          />
+        </ThemedView>
+
+        <ThemedView
+          style={{
+            marginHorizontal: 10,
+            marginVertical: 5,
+            flexDirection: 'row',
+            gap: 10
+          }}
+        >
+          <ThemedTextInput
+            placeholder='Precio'
+            keyboardType='numeric'
+            style={{ flex: 1 }}
+          />
+
+          <ThemedTextInput
+            placeholder='Stock'
+            keyboardType='numeric'
+            style={{ flex: 1 }}
+          />
+        </ThemedView>
+      </ScrollView>
+    </KeyboardAvoidingView>
   )
 }
 
