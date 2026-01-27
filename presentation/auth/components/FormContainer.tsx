b4ba56927a62f78eb16b76fbaa2f2e600879e6f8
Leonardo <leonardo38361@gmail.com>
2026-01-27 19:05:12 -0300

    feat: crear componente FormContainer reutilizable
    - Extraer lógica de Formik en componente genérico FormContainer
    - Componente encapsula configuración de Formik (initialValues, onSubmit, validationSchema)
    - Permite reutilización en múltiples formularios (auth, productos, etc)
    - Mantiene render props pattern para flexibilidad

---
+++ presentation/auth/components/FormContainer.tsx
@@ -0,0 +1,31 @@
+
+import { Formik, FormikHelpers, FormikProps } from 'formik';
+import React from 'react';
+import * as yup from 'yup';
+
+interface Props<T extends Record<string, any>> {
+    initialValues: T;
+    children: (formik: FormikProps<T>) => React.ReactNode;
+    onSubmit: (values: T, formikHelpers: FormikHelpers<T>) => void | Promise<void>;
+    validationSchema?: yup.ObjectSchema<T>;
+
+}
+
+const FormContainer = <T extends object>({ initialValues, children, onSubmit, validationSchema }: Props<T>) => {
+    return (
+        <Formik
+            initialValues={initialValues as T}
+            validationSchema={validationSchema}
+            onSubmit={onSubmit}
+        >
+            {(formikProps) => (
+                <>
+                    {children(formikProps)}
+                </>
+            )}
+        </Formik>
+
+    )
+}
+
+export default FormContainer;
