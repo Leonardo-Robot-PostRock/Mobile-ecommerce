3cffbf9921e771f9e08afb008c1e6daf43a485f1
Leonardo <leonardo38361@gmail.com>
2026-01-29 18:04:08 -0300

    chore: update presentation/camera/index.tsx

---
+++ presentation/camera/index.tsx
@@ -0,0 +1,5 @@
+export { default as FlipCameraButton } from './components/FlipCameraButton';
+export { default as GaleryButton } from './components/GaleryButton';
+export { default as ReturnCancelButton } from './components/ReturnCancelButton';
+export { default as ShutterButton } from './components/ShutterButton';
+
