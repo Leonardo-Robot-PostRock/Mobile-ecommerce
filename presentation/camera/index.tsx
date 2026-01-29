2d703f03b63955e2c76b7466fa51a28ceba59d36
Leonardo <leonardo38361@gmail.com>
2026-01-29 18:51:17 -0300

    refactor(camera): refactor interfaz del componente principal de cámara

--- presentation/camera/index.tsx
+++ presentation/camera/index.tsx
@@ -1,5 +1,7 @@
+export { default as ConfirmImageButton } from './components/ConfirmImageButton';
 export { default as FlipCameraButton } from './components/FlipCameraButton';
 export { default as GaleryButton } from './components/GaleryButton';
+export { default as RetakeImageButton } from './components/RetakeImageButton';
 export { default as ReturnCancelButton } from './components/ReturnCancelButton';
 export { default as ShutterButton } from './components/ShutterButton';
 
