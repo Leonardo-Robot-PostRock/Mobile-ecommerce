c6d6ae1ee33655c04fa236555630f46e316bb771
Leonardo <leonardo38361@gmail.com>
2026-01-29 18:04:08 -0300

    chore: update presentation/camera/components/ReturnCancelButton.tsx

---
+++ presentation/camera/components/ReturnCancelButton.tsx
@@ -0,0 +1,31 @@
+import { Ionicons } from '@expo/vector-icons';
+import React from 'react';
+import { StyleSheet, TouchableOpacity } from 'react-native';
+
+interface Props {
+    onPress: () => void;
+}
+
+const ReturnCancelButton = ({ onPress }: Props) => {
+    return (
+        <TouchableOpacity onPress={onPress} style={styles.returnCancelButton}>
+            <Ionicons name="arrow-back-outline" size={30} color="white" />
+        </TouchableOpacity>
+    )
+}
+
+export default ReturnCancelButton;
+
+const styles = StyleSheet.create({
+    returnCancelButton: {
+        width: 50,
+        height: 50,
+        borderRadius: 32,
+        backgroundColor: '#17202A',
+        position: 'absolute',
+        top: 40,
+        left: 32,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+})
